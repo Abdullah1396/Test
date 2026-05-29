63bc8036926de1de9a81c7a48aa415942431c61d
Abdullah1396 <91352877+Abdullah1396@users.noreply.github.com>
2026-05-29 17:23:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const questions = [
+
 {
 image:"images/5451F0F6-D7AA-41A7-ACA4-27103043D6F8.png",
 question:"أي رابط ظهر في ذهنك أولاً؟",
@@ -8,6 +9,7 @@ answers:[
 {text:"الشجرة + المفتاح",type:"explorer"}
 ]
 },
+
 {
 image:"images/4CB438D8-2975-4F57-9CE3-8638610012E8.png",
 question:"أي رابط ظهر في ذهنك أولاً؟",
@@ -16,49 +18,66 @@ answers:[
 {text:"الشمعة + الجنازة",type:"symbolic"},
 {text:"الساعة + الجنازة",type:"human"}
 ]
+},
+
+{
+image:"images/2DCE45C1-BEC4-449B-8D6D-5940E212F8CB.png",
+question:"أي رابط ظهر في ذهنك أولاً؟",
+answers:[
+{text:"الحقيبة + الفانوس",type:"explorer"},
+{text:"الفانوس + الكرسي",type:"symbolic"},
+{text:"الحقيبة + الكرسي",type:"builder"}
+]
+},
+
+{
+image:"images/B5C20064-5633-4859-8277-41224BD32335.png",
+question:"أي رابط ظهر في ذهنك أولاً؟",
+answers:[
+{text:"القناع + الشمعة",type:"symbolic"},
+{text:"الدرج + الشمعة",type:"builder"},
+{text:"القناع + الدرج",type:"explorer"}
+]
+},
+
+{
+image:"images/EE394658-227B-45D9-9191-4702BB0F9C2B.png",
+question:"أي رابط ظهر في ذهنك أولاً؟",
+answers:[
+{text:"الشخص + الجسر",type:"human"},
+{text:"الجسر + الضباب",type:"builder"},
+{text:"الشخص + الضباب",type:"explorer"}
+]
+},
+
+{
+image:"images/17B21646-A7B8-416D-9582-1396FC95872D.png",
+question:"أي رابط ظهر في ذهنك أولاً؟",
+answers:[
+{text:"المرآة + الكتاب",type:"symbolic"},
+{text:"الكتاب + الباب",type:"builder"},
+{text:"المرآة + الباب",type:"explorer"}
+]
+},
+
+{
+image:"images/3837A93E-B92B-4DA9-A6DD-D59415288121.png",
+question:"أي رابط ظهر في ذهنك أولاً؟",
+answers:[
+{text:"الطفل + المظلة",type:"human"},
+{text:"المظلة + الأرجوحة",type:"builder"},
+{text:"الطفل + الأرجوحة",type:"explorer"}
+]
+},
+
+{
+image:"images/108E05AF-A09E-4DB1-A9D8-40D38EBA0308.png",
+question:"أي رابط ظهر في ذهنك أولاً؟",
+answers:[
+{text:"القفص + المفتاح",type:"symbolic"},
+{text:"المفتاح + النافذة",type:"explorer"},
+{text:"القفص + النافذة",type:"builder"}
+]
 }
+
 ];
-
-let currentQuestion = 0;
-
-function loadQuestion(){
-
-const q = questions[currentQuestion];
-
-document.getElementById("question-container").innerHTML =
-`
-<img src="${q.image}" style="width:100%">
-<h2>${q.question}</h2>
-
-<button onclick="answerQuestion(0)">
-${q.answers[0].text}
-</button>
-
-<button onclick="answerQuestion(1)">
-${q.answers[1].text}
-</button>
-
-<button onclick="answerQuestion(2)">
-${q.answers[2].text}
-</button>
-`;
-}
-
-function answerQuestion(index){
-
-currentQuestion++;
-
-if(currentQuestion < questions.length){
-
-loadQuestion();
-
-}else{
-
-document.getElementById("question-container").innerHTML =
-"<h1>نجح الاختبار</h1>";
-
-}
-
-}
-
-loadQuestion();
