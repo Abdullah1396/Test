a5b831a1592e6e3f7b3ee16a6d7b76350e6d394b
Abdullah1396 <91352877+Abdullah1396@users.noreply.github.com>
2026-05-29 23:26:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
-document.getElementById("question-container").innerHTML = "<h1>اشتغل</h1>";
+function showResult(){
+
+const maxScore = Math.max(
+scores.builder,
+scores.explorer,
+scores.human,
+scores.symbolic
+);
+
+const winners = [];
+
+for (const type in scores){
+if(scores[type] === maxScore){
+winners.push(type);
+}
+}
+
+let title = "";
+
+if(winners.length > 1){
+
+title = "نمط مختلط: ";
+
+if(winners.includes("builder"))
+title += "البنائي ";
+
+if(winners.includes("explorer"))
+title += "الاستكشافي ";
+
+if(winners.includes("human"))
+title += "الإنساني ";
+
+if(winners.includes("symbolic"))
+title += "الرمزي ";
+
+}else{
+
+const result = winners[0];
+
+if(result==="builder")
+title="النمط البنائي";
+
+if(result==="explorer")
+title="النمط الاستكشافي";
+
+if(result==="human")
+title="النمط الإنساني";
+
+if(result==="symbolic")
+title="النمط الرمزي";
+
+}
+
+container.innerHTML = `
+<h1>${title}</h1>
+`;
+}
