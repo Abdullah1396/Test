69c7b57d43d920a9182530bab9ac33f63bbf338a
Abdullah1396 <91352877+Abdullah1396@users.noreply.github.com>
2026-05-29 17:01:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,4 @@
 const questions = [
-
-{
-image:"images/108E05AF-A09E-4DB1-A9D8-40D38EBA0308.png",
-question:"أي رابط ظهر في ذهنك أولاً؟",
-answers:[
-{text:"القفص + المفتاح",type:"symbolic"},
-{text:"المفتاح + النافذة",type:"explorer"},
-{text:"القفص + النافذة",type:"builder"}
-]
-},
-
-{
-image:"images/17B21646-A7B8-416D-9582-1396FC95872D.png",
-question:"أي رابط ظهر في ذهنك أولاً؟",
-answers:[
-{text:"المرآة + الكتاب",type:"symbolic"},
-{text:"الكتاب + الباب",type:"builder"},
-{text:"المرآة + الباب",type:"explorer"}
-]
-},
-
-{
-image:"images/2DCE45C1-BEC4-449B-8D6D-5940E212F8CB.png",
-question:"أي رابط ظهر في ذهنك أولاً؟",
-answers:[
-{text:"الحقيبة + الفانوس",type:"explorer"},
-{text:"الفانوس + الكرسي",type:"symbolic"},
-{text:"الحقيبة + الكرسي",type:"builder"}
-]
-},
-
-{
-image:"images/3837A93E-B92B-4DA9-A6DD-D59415288121.png",
-question:"أي رابط ظهر في ذهنك أولاً؟",
-answers:[
-{text:"الطفل + المظلة",type:"human"},
-{text:"المظلة + الأرجوحة",type:"builder"},
-{text:"الطفل + الأرجوحة",type:"explorer"}
-]
-},
-
-{
-image:"images/4CB438D8-2975-4F57-9CE3-8638610012E8.png",
-question:"أي رابط ظهر في ذهنك أولاً؟",
-answers:[
-{text:"الساعة + الشمعة",type:"builder"},
-{text:"الشمعة + الجنازة",type:"symbolic"},
-{text:"الساعة + الجنازة",type:"human"}
-]
-},
-
 {
 image:"images/5451F0F6-D7AA-41A7-ACA4-27103043D6F8.png",
 question:"أي رابط ظهر في ذهنك أولاً؟",
@@ -59,27 +8,15 @@ answers:[
 {text:"الشجرة + المفتاح",type:"explorer"}
 ]
 },
-
 {
-image:"images/B5C20064-5633-4859-8277-41224BD32335.png",
+image:"images/4CB438D8-2975-4F57-9CE3-8638610012E8.png",
 question:"أي رابط ظهر في ذهنك أولاً؟",
 answers:[
-{text:"القناع + الشمعة",type:"symbolic"},
-{text:"الشمعة + الدرج",type:"human"},
-{text:"القناع + الدرج",type:"explorer"}
-]
-},
-
-{
-image:"images/EE394658-227B-45D9-9191-4702BB0F9C2B.png",
-question:"أي رابط ظهر في ذهنك أولاً؟",
-answers:[
-{text:"الشخص + الجسر",type:"human"},
-{text:"الجسر + الضباب",type:"builder"},
-{text:"الشخص + الضباب",type:"explorer"}
+{text:"الساعة + الشمعة",type:"builder"},
+{text:"الشمعة + الجنازة",type:"symbolic"},
+{text:"الساعة + الجنازة",type:"human"}
 ]
 }
-
 ];
 
 let currentQuestion = 0;
@@ -98,17 +35,17 @@ function loadQuestion(){
 
 const q = questions[currentQuestion];
 
-container.innerHTML =  <img src="${q.image}">  <h2>${q.question}</h2>  ${q.answers.map(answer =>
+container.innerHTML = `
+<img src="${q.image}">
+<h2>${q.question}</h2>
 
+${q.answers.map(answer=>`
 <div class="choice"
 onclick="selectAnswer('${answer.type}')">
-
 ${answer.text}
-
 </div>
-
-).join("")}  ;
-
+`).join("")}
+`;
 }
 
 function selectAnswer(type){
@@ -118,53 +55,28 @@ scores[type]++;
 currentQuestion++;
 
 if(currentQuestion < questions.length){
-
 loadQuestion();
-
 }else{
-
 showResult();
-
 }
 
 }
 
 function showResult(){
 
-let result =
-Object.keys(scores).reduce((a,b)=>
-scores[a] > scores[b] ? a : b
-);
+let result = Object.keys(scores)
+.reduce((a,b)=>scores[a]>scores[b]?a:b);
 
 let title = "";
-let description = "";
 
-if(result==="builder"){
-title="النمط البنائي";
-description="أنت تميل لرؤية الأنظمة والعلاقات المنطقية والبنية الكامنة خلف الأشياء.";
+if(result==="builder") title="النمط البنائي";
+if(result==="explorer") title="النمط الاستكشافي";
+if(result==="human") title="النمط الإنساني";
+if(result==="symbolic") title="النمط الرمزي";
+
+container.innerHTML = `
+<h1>${title}</h1>
+`;
 }
 
-if(result==="explorer"){
-title="النمط الاستكشافي";
-description="أنت مدفوع بالفضول واكتشاف الاحتمالات والمسارات الجديدة.";
-}
-
-if(result==="human"){
-title="النمط الإنساني";
-description="أنت ترى العالم من خلال الأشخاص والمشاعر والتجارب الإنسانية.";
-}
-
-if(result==="symbolic"){
-title="النمط الرمزي";
-description="أنت تميل إلى قراءة المعاني الخفية والرموز والاستعارات وراء الأشياء.";
-}
-
-container.innerHTML = "";
-
-
-
-document.getElementById("result").innerHTML = <h1>${title}</h1> <p>${description}</p>;
-
-}
-
-loadQues
+loadQuestion();
